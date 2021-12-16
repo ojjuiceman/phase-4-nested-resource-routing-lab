0badc568b8b74986147d722322bcc0537dac064b
Okera Johnson <okerajohnson@gmail.com>
2021-12-16 11:49:30 -0500

    commit tree 78a267a421d67d86e61ac49069a0a140ee16a499

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,80 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
+
+  # def index
+  #   items = Item.all
+  #   render json: items, include: :user
+  # end
+
+  # http://facebook.com/items
+
+def index
+  if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+  else
     items = Item.all
-    render json: items, include: :user
+  end
+  render json: items, include: :user
+end
+
+
+def show
+  if params[:user_id]
+  user = User.find(params[:user_id])
+ item = user.items.find(params[:id])
+  else 
+    item = Item.find(params[:id])
+  end
+ render json: item, include: :user
+
+end
+
+ def create
+    user = find_user
+    item = user.Item.create(item_params)
+    render json: item, status: :created
+ end
+
+private
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
+
+
+
+
+
+
+
+# class DogHousesController < ApplicationController
+#   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+#   def show
+#     dog_house = DogHouse.find(params[:id])
+#     render json: dog_house
+#   end
+
+#   private
+
+#   def render_not_found_response
+#     render json: { error: "Dog house not found" }, status: :not_found
+#   end
+
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show, :create]
+  end
 end
+
+
+# Rails.application.routes.draw do
+
+#   resources :dog_houses, only: [:show] do
+#     # nested resource for reviews
+#     resources :reviews, only: [:show, :index]
+#   end
+
+#   resources :reviews, only: [:show, :index, :create]
+# end
